2b6fd16cf68b75f230103ee46549e88a036ca632
Dasun Pubudumal <pubudumald@gmail.com>
2025-08-11 16:27:55 +0100

    feat(linter): implement configuration parsing for NoWarningComments rule

--- crates/oxc_linter/src/rules/eslint/no_warning_comments.rs
+++ crates/oxc_linter/src/rules/eslint/no_warning_comments.rs
@@ -3,7 +3,11 @@ use oxc_macros::declare_oxc_lint;
 use crate::{AstNode, context::LintContext, rule::Rule};
 
 #[derive(Debug, Default, Clone)]
-pub struct NoWarningComments;
+pub struct NoWarningComments {
+    terms: Option<Vec<String>>,
+    decorations: Option<Vec<String>>,
+    location: Option<String>,
+}
 
 // See <https://github.com/oxc-project/oxc/issues/6050> for documentation details.
 declare_oxc_lint!(
@@ -68,6 +72,41 @@ impl Rule for NoWarningComments {
         //     - adds the comment to the comment that will be displayed
         //     - if the line is longer than 40 characters it will be truncated with an ellipsis
         //     - it will report the message to the context with messageId unexpectedCooment with the data being the matched term and the comment but if it is too long it is truncated to 40 characters with an ellipsis
+    }
+
+    fn from_configuration(value: serde_json::Value) -> Self {
+        // Read the configuration for term, decoration and location from _value and then
+        // return NoWarningComments {} struct with the attributes terms, decoration and locations.
+
+        // TODO: Create. NoWarningCommentsConfig struct and box it inside the return struct NoWarningCommentsConfig
+        // See crates/oxc_linter/src/rules/eslint/default_case.rs.
+        let mut terms: Option<Vec<String>> = None;
+        let mut decorations: Option<Vec<String>> = None;
+        let mut location: Option<String> = None;
+
+        if let Some(config) = value.get(0) {
+            if let Some(terms_config) = config.get("terms") {
+                terms = terms_config.as_array().map(|arr| {
+                    arr.iter()
+                        .filter_map(|v| v.as_str().map(|s| s.to_string()))
+                        .collect::<Vec<String>>()
+                });
+            }
+
+            if let Some(decorations_config) = config.get("decorations") {
+                decorations = decorations_config.as_array().map(|arr| {
+                    arr.iter()
+                        .filter_map(|v| v.as_str().map(|s| s.to_string()))
+                        .collect::<Vec<String>>()
+                });
+            }
+
+            if let Some(location_config) = config.get("location") {
+                location = location_config.as_str().map(|s| s.to_string());
+            }
+        }
+
+        return Self { terms, decorations, location };
     }
 }
 
